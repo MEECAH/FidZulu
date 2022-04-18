9fe6e0b72dd4409a94c50b3e5226f4746a009cf8
dhruvenkumarChauhan <85200767+dhruvenkumarChauhan@users.noreply.github.com>
2022-04-18 16:56:51 -0400

    test

--- frontEnd/fid-zulu/src/app/app.module.ts
+++ frontEnd/fid-zulu/src/app/app.module.ts
@@ -3,6 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+<<<<<<< Updated upstream
+=======
+import { BikesListComponent } from './bikes-list/bikes-list.component';
+import { DVDsListComponent } from './dvds-list/dvds-list.component';
+import { FoodsListComponent } from './foods-list/foods-list.component';
+import { LaptopsListComponent } from './laptops-list/laptops-list.component';
+import { BooksListComponent } from './books-list/books-list.component';
+import { ToysListComponent } from './toys-list/toys-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ShowAllListsComponent } from './show-all-lists/show-all-lists.component';
+import { HttpClientModule } from '@angular/common/http';
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
@@ -10,7 +22,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+<<<<<<< Updated upstream
     AppRoutingModule
+=======
+    AppRoutingModule,
+    HttpClientModule
+>>>>>>> Stashed changes
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontEnd/fid-zulu/src/app/products/product.service.ts
@@ -0,0 +1,180 @@
+import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { Bike } from '../models/bike.model';
+import { Dvd } from '../models/dvd.model';
+import { Food } from '../models/food';
+import { Laptop } from '../models/laptop';
+import { Toys } from '../models/toys';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  
+  mockBikes: Bike[] = [];
+
+  mockDvds: Dvd[] = [
+    {
+      title: 'Avengers - Infinity War',
+      mpaa_rating: 'PG-13',
+      studio: 'MARVEL',
+      time: 149,
+      price: 18.55,
+    },
+    {
+      title: 'Spider-Man Homecoming',
+      mpaa_rating: '14 and over',
+      studio: 'Sony Pictures Home Entertainment',
+      time: 133,
+      price: 7.23,
+    },
+    {
+      title: 'Ant-Man',
+      mpaa_rating: 'PG-13',
+      studio: 'Walt Disney Video',
+      time: 117,
+      price: 19.98,
+    },
+    {
+      title: 'Captain America',
+      mpaa_rating: 'PG',
+      studio: 'Walt Disney Video',
+      time: 123,
+      price: 24.88,
+    },
+  ];
+
+  mockFood: Food[] =
+  [
+    {
+      "name": "The Original Sandwich",
+      "brand": "Oreo",
+      "weight": "303g",
+      "calories": 405,
+      "price": 2.85
+    },
+    {
+      "name": "Peanut Butter",
+      "brand": "KRAFT",
+      "weight": "2000g",
+      "calories": 726,
+      "price": 9.39
+    },
+    {
+      "name": "Beef Ravioli",
+      "brand": "Chef Boyardee",
+      "weight": "425g",
+      "calories": 250,
+      "price": 2.45
+    },
+    {
+      "name": "Medium Cheddar Cheese",
+      "brand": "MOON CHEESE",
+      "weight": "57g",
+      "calories": 525,
+      "price": 5.87
+    }
+  ];
+
+  mockToys : Toys[] = [
+    {
+      "name": "Medical Kit",
+      "brand": "Fisher-Price",
+      "ageGroup": "3 to 9",
+      "prize": 20.41
+    },
+    {
+      "name": "Ferry Boat",
+      "brand": "Green Toys",
+      "ageGroup": "3 to 6",
+      "prize": 13.26
+    },
+    {
+      "name": "Rock-a-Stack",
+      "brand": "Fisher-Price",
+      "ageGroup": "1 to 5",
+      "prize": 5.99
+    },
+    {
+      "name": "Stack Up Cups",
+      "brand": "The First Years",
+      "ageGroup": "0 to 3",
+      "prize": 3.99
+    }
+  ]
+
+  mockLaptops: Laptop[] = [
+    {
+      "product": "ThinkPad T430s",
+      "brand": "Lenovo",
+      "CPU": "core i5-3320",
+      "memory": "8GB",
+      "price": 325.09
+    },
+    {
+      "product": "MacBook Air",
+      "brand": "Apple",
+      "CPU": "core i5 1.6GHz",
+      "memory": "4GB",
+      "price": 621.78
+    },
+    {
+      "product": "Ideapad 330",
+      "brand": "Lenovo",
+      "CPU": "core i3-8130U",
+      "memory": "4GB",
+      "price": 459.98
+    },
+    {
+      "product": "MacBook Pro",
+      "brand": "Apple",
+      "CPU": "core i5 2.5GHz",
+      "memory": "4GB",
+      "price": 2999.99
+    }
+  ]
+
+  // getStocks() : Observable<Stock[]>  {
+  //   return this.http.get<Stock[]>(this.url).pipe(catchError(this.handleError));
+  // }
+
+  url ="localhost:3021/bikes/Durham";
+
+  handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+        // A client-side or network error occurred. Handle it.
+        console.error('An error occurred:', error.error.message);
+    } else {
+        // The backend returned an unsuccessful response code.
+        // The response body may contain clues 
+        console.error(`Backend returned code ${error.status}, body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(() => 'Unable to contact service; please try again later.');
+};
+
+  getBikes(): Observable<Bike[]> {
+    return this.http.get<Bike[]>(this.url)
+    .pipe(catchError(this.handleError));
+  }
+
+  getDvds(): Dvd[] {
+    return this.mockDvds;
+  }
+
+  getFood() : Food[] {
+    return this.mockFood
+  }
+
+  getToys(): Toys[]{
+    return this.mockToys;
+  }
+
+  getLaptops(): Laptop[]{
+    return this.mockLaptops;
+  }
+
+  constructor(private http: HttpClient) { }
+}
